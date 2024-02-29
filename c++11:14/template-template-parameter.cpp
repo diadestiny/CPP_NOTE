5db010bb5aaf66ae6c6720f0dfd3b3862e2e132c
diadestiny <44188454+diadestiny@users.noreply.github.com>
2024-02-29 22:16:09 +0800

    add comment

--- c++11:14/template-template-parameter.cpp
+++ c++11:14/template-template-parameter.cpp
@@ -1,6 +1,19 @@
 #include <vector>
 using namespace std;
 
+// 什么场景需要用到模板模板参数？
+// 比如vector<T,allocator<T>> 
+
+
+// 首先XCLs是一个模板类：
+// 1. 希望内部的容器成员的属性可以是任意类型
+template<typename T>
+class XCLs_0{
+private:
+        vector<T> c;
+}
+// 2. 希望内部的容器成员c可以适配多种容器，比如vector, list, array等
+// XCls所以需要指定第二个模板参数(原始的话是typename Container), 如果这样的话，成员Container<T> 是不能通过的，因为有些Container是需要指定内部的第二个模板参数的，所以需要引入模板模板参数
 template<typename T,
         template<class>
         class Container
